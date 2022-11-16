83d4bd128ccb46229ffd783ef99ae2b7f1b4760d
Harry-KNIGHT <eliott.knight@icloud.com>
2022-11-16 17:42:51 +0100

    Add stop live activity

--- LiveActivitiesButtonTest/ContentView.swift
+++ LiveActivitiesButtonTest/ContentView.swift
@@ -12,39 +12,45 @@ struct ContentView: View {
 	@State private var timer: Date?
 
 	@State private var activity: Activity<ButtonActivityTestAttributes>?
-    var body: some View {
-        VStack {
+	var body: some View {
+		VStack {
 			if let dateTimer = timer {
 				Text(dateTimer, style: .timer)
-
+					.font(.title)
 			}
-				HStack(spacing: 30) {
-					Button {
+
+			HStack(spacing: 30) {
+				Button {
+					if timer != nil {
+						guard let timer else { return }
+						let state = ButtonActivityTestAttributes.ContentState(timer: timer)
+						Task {
+							await activity?.end(using: state, dismissalPolicy: .immediate)
+						}
+						self.timer = nil
+
+					} else {
 						timer = .now
 						guard let timer else { return }
 						let update =  ButtonActivityTestAttributes.ContentState(timer: timer)
 						let attributes = ButtonActivityTestAttributes()
 						activity = try? Activity<ButtonActivityTestAttributes>.request(attributes: attributes, contentState: update)
-
-
-					} label: {
-						Label("Start session", systemImage: "play.fill")
 					}
 
-					Button(action: {
-						timer = nil
-					}, label: {
-						Label("Stop", systemImage: "pause.fill")
-					})
+				} label: {
+					Label(timer == nil ? "Start" : "Stop", systemImage: timer == nil ? "play.fill" : "pause.fill")
+						.font(.title)
+				}
+				.buttonStyle(.borderedProminent)
+				.buttonBorderShape(.capsule)
 			}
-        }
-
-        .padding()
-    }
+		}
+		.padding()
+	}
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+	static var previews: some View {
+		ContentView()
+	}
 }
